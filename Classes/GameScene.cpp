7cb49e0da1ed69eccf4bd3f1cdc009709decfd4b
chensm9 <2540740154@qq.com>
2018-05-21 17:37:27 +0800

    :bug:使用Layer，实现两个Layer上元素世界坐标和本地坐标转换

--- Classes/GameScene.cpp
+++ Classes/GameScene.cpp
@@ -30,28 +30,38 @@ bool GameSence::init()
     bg->setPosition(Vec2(visibleSize.width / 2 + origin.x, visibleSize.height / 2 + origin.y));
     this->addChild(bg, 0);
 
+    stoneLayer = Layer::create();
+    stoneLayer->setPosition(Vec2(0, 0));
+    stoneLayer->setAnchorPoint(Point(0, 0));
+    this->addChild(stoneLayer, 2);
+
+    mouseLayer = Layer::create();
+    mouseLayer->setPosition(Vec2(0, visibleSize.height / 2 + origin.y));
+    mouseLayer->setAnchorPoint(Point(0, 0));
+    this->addChild(mouseLayer, 2);
+
     stone = Sprite::create("stone.png");
-    stone->setPosition(Vec2(visibleSize.width / 2 + origin.x + 80, visibleSize.height + origin.y - 160));
-    this->addChild(stone, 1);
+    stone->setPosition(Vec2(560, 480));
+    stoneLayer->addChild(stone, 1);
 
     mouse = Sprite::createWithSpriteFrameName("pulled-gem-mouse-0.png");
     Animate* mouseAnimate = Animate::create(AnimationCache::getInstance()->getAnimation("mouseAnimation"));
     mouse->runAction(RepeatForever::create(mouseAnimate));
-    mouse->setPosition(Vec2(visibleSize.width / 2 + origin.x, visibleSize.height / 2 + origin.y - 20));
-    this->addChild(mouse, 2);
+    mouse->setPosition(Vec2(visibleSize.width / 2 + origin.x, 0));
+    mouseLayer->addChild(mouse, 1);
 
     auto shoot = MenuItemLabel::create(
-            Label::createWithTTF("Shoot", "fonts/arial.ttf", 60),
-            CC_CALLBACK_1(GameSence::Shoot, this));
+        Label::createWithTTF("Shoot", "fonts/arial.ttf", 60),
+        CC_CALLBACK_1(GameSence::Shoot, this));
     shoot->setPosition(Vec2(visibleSize.width / 2 + origin.x + 300, visibleSize.height + origin.y - 160));
     auto menu = Menu::create(shoot, NULL);
     menu->setPosition(Vec2::ZERO);
     this->addChild(menu, 2);
-	return true;
+    return true;
 }
 
 bool GameSence::onTouchBegan(Touch *touch, Event *unused_event) {
-	auto location = touch->getLocation();
+    auto location = touch->getLocation();
 
     auto cheese = Sprite::create("cheese.png");
     cheese->setPosition(location);
@@ -62,33 +72,36 @@ bool GameSence::onTouchBegan(Touch *touch, Event *unused_event) {
     });
     cheese->runAction(Sequence::create(fadeOut, clearUp, nullptr));
 
-    auto moveTime = location.getDistance(mouse->getPosition()) / 100 * 0.5;
-    auto mouseMoveTo = MoveTo::create(moveTime, location);
+    auto moveTime = location.getDistance(mouseLayer->convertToWorldSpace(mouse->getPosition())) / 100 * 0.5;
+    auto mouseMoveTo = MoveTo::create(moveTime, mouseLayer->convertToNodeSpace(location));
     mouse->runAction(mouseMoveTo);
-	return true;
+    return true;
 }
 
 void GameSence::Shoot(Ref* pSender) {
-    auto diamond = Sprite::createWithSpriteFrameName("pulled-diamond-0.png");
+    /*auto diamond = Sprite::createWithSpriteFrameName("pulled-diamond-0.png");
     Animate* diamondAnimate = Animate::create(AnimationCache::getInstance()->getAnimation("diamondAnimation"));
-    diamond->runAction(RepeatForever::create(diamondAnimate));
-    diamond->setPosition(mouse->getPosition());
+    diamond->runAction(RepeatForever::create(diamondAnimate));*/
+    auto diamond = Sprite::create("diamond.png");
+    diamond->setPosition(mouseLayer->convertToWorldSpace(mouse->getPosition()));
     this->addChild(diamond, 2);
 
     auto stone2 = Sprite::create("stone.png");
     stone2->setPosition(stone->getPosition());
-    this->addChild(stone2, 1);
-    auto moveTo = MoveTo::create(1, mouse->getPosition());
+    stoneLayer->addChild(stone2, 1);
+    auto moveTo = MoveTo::create(1, mouseLayer->convertToWorldSpace(mouse->getPosition()));
     auto fadeOut = FadeOut::create(0.5f);
     auto clearUp = CallFunc::create([stone2, this]() {
-        this->removeChild(stone2);
+        this->stoneLayer->removeChild(stone2);
     });
     auto seq = Sequence::create(moveTo, fadeOut, clearUp, nullptr);
     stone2->runAction(seq);
 
     Size visibleSize = Director::getInstance()->getVisibleSize();
     Vec2 origin = Director::getInstance()->getVisibleOrigin();
-    MoveTo* moveTo2 = MoveTo::create(1, Vec2(origin.x + random() % (int(visibleSize.width)),
-                                         origin.y + random() % (int(visibleSize.height))));
+    auto randomPosition = mouseLayer->convertToNodeSpace(
+        Vec2(origin.x + random() % (int(visibleSize.width)),
+             origin.y + random() % (int(visibleSize.height))));
+    MoveTo* moveTo2 = MoveTo::create(1, randomPosition);
     mouse->runAction(moveTo2);
 }
